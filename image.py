3f9bd68c901042b1409e28cbbc3c92bf7660b281
Owen Lee Scott <Fumwa23@outlook.com>
2023-11-28 00:33:39 +0000

    Created function which take prompt and provider letting you choose between DALL-E and Stable diffusion

--- image.py
+++ image.py
@@ -23,6 +23,8 @@ load_dotenv()
 # Variables that get the keys from .env
 openai.api_key = os.environ["OPENAI_API_KEY"]
 client = OpenAI()
+
+
 dall_e_3_key = os.getenv("DALL-E-3_KEY")  # work in progress
 stable_diffusion_key = os.getenv("STABLE_DIFFUSION_KEY")  # work in progress
 midjourney_key = os.getenv(
@@ -30,18 +32,39 @@ midjourney_key = os.getenv(
 )  # Not available for developers as of 27/11/2023
 stability_ai_key = os.getenv("STABILITY_AI_API_KEY")
 
-
+#use this function to generate images
 def generate_image(prompt, provider, size="1024x1024"):
-    if provider == "OPENAI":  # if loop to pick provider
-        openai.api_key = os.getenv("OPENAI_API_KEY")
+    '''
+    Call this function to generate an image.
+    Input a prompt and a provider
+    Provider options: "STABILITY_AI", "DALL-E-3"
+    '''
+    
+    if provider == "DALL-E-3":  # if loop to pick provider
+        openai.api_key = os.getenv("OPENAI_API_KEY") # I think this line is redundant 
+        img = generate_dali_image(prompt=prompt, size=size, quality="standard", n=1)
+        return img
+        
     elif provider == "STABILITY_AI":
-        stability_ai_key = os.getenv("STABILITY_AI_API_KEY")
-    elif provider == "DALL-E-3":
-        dall_e_3_key = os.getenv("DALL-E-3_KEY")
-    elif provider == "STABLE_DIFFUSION":
-        stable_diffusion_key = os.getenv("STABLE_DIFFUSION-KEY")
-    elif provider == "MIDJOURNEY":
-        midjourney_key = os.getenv("MIDJOURNEY_KEY")
+        #stability_ai_key = os.getenv("STABILITY_AI_API_KEY")
+        img = generate_stable_diffusion_image(prompt=prompt)
+        return img
+    #the rest of these are not relevant yet
+    #//@lizziem
+    # elif provider == "OPENAI":
+    #     dall_e_3_key = os.getenv("DALL-E-3_KEY")
+    # elif provider == "STABLE_DIFFUSION":
+    #     stable_diffusion_key = os.getenv("STABLE_DIFFUSION-KEY")
+    # elif provider == "MIDJOURNEY":
+    #     midjourney_key = os.getenv("MIDJOURNEY_KEY")
+    #//@lizziem
+
+
+def generate_stable_diffusion_image(prompt, size="1024x1024"):
+    '''
+    This function generates an image using stable diffusion.
+    '''
+
 
     height = int(size.split("x")[0])
     width = int(size.split("x")[1])
@@ -80,58 +103,47 @@ def generate_image(prompt, provider, size="1024x1024"):
 
     save_fp = "temp.png"
     for i, image in enumerate(data["artifacts"]):
-        print("Image generated")
         with open(save_fp, "wb") as f:
             f.write(base64.b64decode(image["base64"]))
+            print('Image generated')
         break
 
     img = Image.open(save_fp)
-    # os.remove(save_fp)
+    img.show()
     os.remove(save_fp)
     return img
 
 
-# //Fumwa23
-# def generate_dali_image(prompt, size="1024x1024", quality="standard", n=1):
-#     """
-#     This function generates an image using the DALI api. The only required input is the prompt,
-#     but if you want you can also adjust size, quality, and number of images generated.
-#     """
 
-#     client = OpenAI()
+def generate_dali_image(prompt, size="1024x1024", quality="standard", n=1):
+    """
+    This function generates an image using the DALI api. The only required input is the prompt,
+    but if you want you can also adjust size, quality, and number of images generated.
+    """
 
-#     response = client.images.generate(
-#         model="dall-e-3",
-#         # Dall E automatically embelishes your prompt. This is how they suggest keeping it short.
-#         # The reason for me doing this is I suspect it uses less tokens to have a shorter prompt.
-#         prompt="I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: "
-#         + prompt,
-#         size=size,
-#         quality=quality,
-#         n=n,
-#     )
-#     print(f"DEBUG: {response}")
+    client = OpenAI()
 
-#     # code works, response gives back a url
-#     # still need to add additional functions to save the image to a file for later use.
-#     image_url = response.data[0].url
+    response = client.images.generate
+        model="dall-e-3",
+        # Dall E automatically embelishes your prompt. This is how they suggest keeping it short.
+        # The reason for me doing this is I suspect it uses less tokens to have a shorter prompt.
+        prompt="I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: "
+        + prompt,
+        size=size,
+        quality=quality,
+        n=n,
+    )
+    print(f"DEBUG: {response}")
 
-#     return image_url
-#     # //Fumwa23
+    # code works, response gives back a url
+    # still need to add additional functions to save the image to a file for later use.
+    image_url = response.data[0].url
+
+    return image_url
 
 
 if __name__ == "__main__":
-    response = generate_image(" A nebula.", provider="STABILITY_AI")
+    response = generate_image("a dragon eating a crocodile", provider="STABILITY_AI")
+    print(response)
 
-    # //Fumwa23
-    # testing dali image generation
-    # response = generate_dali_image(
-    #     prompt="a black and white cat"
-    # )
-    # currently need to copy response into browser to view image
-    # print(f"DEBUG: {response}")
-
-    # the below line does not work
-    # response.open()
-    # //Fumwa23
 # %%
