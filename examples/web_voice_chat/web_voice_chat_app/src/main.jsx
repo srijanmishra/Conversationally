199cca284b2856dabd3d75af44ce271b8719c6f5
life-efficient <harryaberg@gmail.com>
2023-12-03 22:33:34 +0000

    Enables login button which redirects to universal login

--- examples/web_voice_chat/web_voice_chat_app/src/main.jsx
+++ examples/web_voice_chat/web_voice_chat_app/src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+import LoginButton from './components/Login.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-hcutf1dzts3o8n8w.us.auth0.com"
+      clientId="3GDn1MwPdZerVNra7SzxjEFfNH0dCaFB"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <LoginButton />
+      yooo
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
 )
