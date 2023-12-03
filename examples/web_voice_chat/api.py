b9e21081216ef1ba8dd65c63e015de980a72c62e
life-efficient <harryaberg@gmail.com>
2023-12-03 19:32:23 +0000

    removes CORS origin "*" which seemed to be duplicated in the lambda response (now relies only on being specified in lambda

--- examples/web_voice_chat/api.py
+++ examples/web_voice_chat/api.py
@@ -28,7 +28,7 @@ handler = Mangum(api)
 
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
