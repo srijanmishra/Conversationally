de9488682469c53352e9cfd2cb49c1ca1c922b55
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-12-06 20:05:38 +0000

    Adds button, h1 and p styling

--- examples/web_voice_chat/web_voice_chat_app/src/pages/LandingPage.jsx
+++ examples/web_voice_chat/web_voice_chat_app/src/pages/LandingPage.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import LoginButton from '../components/Login.jsx';
-
+import LoginButton from "../components/Login.jsx";
 
 export const LandingPage = () => {
-    return  <>
-        <h1>Build Custom Voice AI Agents</h1>
-        <LoginButton />
-    </>   
-}
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center mb-5">
+            <h1>Build Custom Voice AI Agents</h1>
+          </div>
+          <div className="col-12 text-center">
+            <LoginButton />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
