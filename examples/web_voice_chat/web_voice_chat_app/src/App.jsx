eac06a31f4ad50c3f5fef1b406ec476fa3c83c2e
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2023-12-06 20:17:15 +0000

    Adds settings page

--- examples/web_voice_chat/web_voice_chat_app/src/App.jsx
+++ examples/web_voice_chat/web_voice_chat_app/src/App.jsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { LandingPage } from './pages/LandingPage.jsx';
-import { ChatPage } from './pages/ChatPage.jsx';
+import { LandingPage } from "./pages/LandingPage";
+import { ChatPage } from "./pages/ChatPage";
+import { Settings } from "./pages/Settings";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LandingPage />} />
         <Route path="/" element={<ChatPage />} />
-        {/* <Route path="/settings" element={<Settings />} /> */}
+        <Route path="/login" element={<LandingPage />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </>
   );
-}
+};
 
-export default App
+export default App;
--- examples/web_voice_chat/web_voice_chat_app/src/layout.jsx
+++ examples/web_voice_chat/web_voice_chat_app/src/layout.jsx
@@ -1,5 +1,11 @@
-import { Sun, Settings, User } from "react-feather";
+import {
+  Sun as SunIcon,
+  Settings as SettingsIcon,
+  User as UserIcon,
+  MessageCircle as MessageCircleIcon,
+} from "react-feather";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Layout = ({ children }) => {
   return (
@@ -7,12 +13,19 @@ const Layout = ({ children }) => {
       <nav>
         <div className="container">
           <div className="row">
-            <div className="col-6">
-              <Sun />
+            <div className="col-4">
+              <SunIcon />
             </div>
-            <div className="col-6 text-right">
-              <User className="mr-2" />
-              <Settings />
+            <div className="col-4 text-center">
+              <Link to="/">
+                <MessageCircleIcon />
+              </Link>
+            </div>
+            <div className="col-4 text-right">
+              <UserIcon className="mr-2" />
+              <Link to="/settings">
+                <SettingsIcon />
+              </Link>
             </div>
           </div>
         </div>
