128b8aaf43f7b9fb51cf5d51c2ec6c5f4b3d5c39
life-efficient <harryaberg@gmail.com>
2023-12-10 12:33:04 +0000

    Adds theme-applied background color to dialog

--- conversationally/frontend/src/styles/theme.jsx
+++ conversationally/frontend/src/styles/theme.jsx
@@ -1,23 +1,43 @@
 import { createTheme } from '@mui/material/styles';
 
+const primary = '#A8C6F5';
+const secondary = '#fcfaeb';
+const background = '#343541';
 
-export default createTheme({
-  palette: {
-    mode: 'light',
-    background: {
-        default: '#343541',
+const theme = createTheme({
+    palette: {
+        mode: 'light',
+        background: {
+            default: '#343541',
+        },
+        primary: {
+            main: primary,
+        },
+        secondary: {
+            main: secondary,
+        },
     },
-    primary: {
-        main: '#A8C6F5',
+    typography: {
+        fontFamily: '"Poppins"',
+        allVariants: {
+        color: secondary,
+        },
     },
-    secondary: {
-        main: '#fcfaeb',
-    },
-  },
-  typography: {
-    fontFamily: '"Poppins"',
-    allVariants: {
-      color: '#fcfaeb',
-    },
-  },
+    components: {
+        MuiDialog: {
+            defaultProps: {
+                sx: { //You can copy the code below in your theme
+                    // background: '#000',
+                    '& .MuiPaper-root': {
+                        background: background
+                    },
+                    // '& .MuiBackdrop-root': {
+                    // backgroundColor: 'transparent' // Try to remove this to see the result
+                    // }
+                }
+            }
+        }
+    }
 });
+
+export default theme;
