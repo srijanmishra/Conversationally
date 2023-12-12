d12d4ae181d53a6182b2b51ae8f630ddaddf8d1b
life-efficient <harryaberg@gmail.com>
2023-12-12 21:40:45 +0000

    Adds auto redirect to homepage to navigate to /Conversationally

--- conversationally/frontend/src/App.jsx
+++ conversationally/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { LandingPage } from "./pages/LandingPage";
 import { ChatPage } from "./pages/ChatPage";
 import { Settings } from "./pages/Settings";
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <>
     <Routes>
+      <Route path="/" element={<Navigate to="/Conversationally" />} />
       <Route path="Conversationally">
           <Route path="" element={<ChatPage />} />
           <Route path="login" element={<LandingPage />} />
