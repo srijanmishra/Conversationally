bc2ed864de14cd95b93ea33c2604a2700836614d
life-efficient <harryaberg@gmail.com>
2023-12-17 18:09:03 +0000

    Implements almost-perfect recording audio waveform visualiser

--- conversationally/frontend/src/components/UserActionButton/UserActionButton.jsx
+++ conversationally/frontend/src/components/UserActionButton/UserActionButton.jsx
@@ -1,19 +1,61 @@
 import { Mic as MicIcon, Square as SquareIcon } from "react-feather";
 import "./UserActionButton.scss";
 import PropTypes from "prop-types";
-import { Typography } from "@mui/material";
+import { Grow, Typography, useTheme } from "@mui/material";
 import { LiveAudioVisualizer } from 'react-audio-visualize';
 import { useState } from 'react';
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder';
 
 
 const UserActionButton = (props) => {
+    const theme = useTheme();
 
+    const listeningStyles = props.status === "listening" ? {
+        width:  "500px",
+        padding: "10px",
+    } : {
+        width:  "100px",
+    }
+    
     const styles = {
-        "display": "flex",
-        "flexDirection": "column",
-        "alignItems": "center",
-        "justifyContent": "center",
-        textAlign: "center",
+        container: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+        },
+        recorder: {
+            backgroundColor: "lightgrey",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "row-reverse",
+            height: "100px",
+            marginBottom: "20px",
+            transitionDuration: "0.5s",
+            borderRadius: "50px",
+            overflow: "hidden",
+            margin: "20px",
+            ...listeningStyles,
+        },
+        recorderWaveformContainer: {
+            transitionDuration: "0.5s",
+            overflow: "hidden",
+            width: props.status === "listening" ? "400px" : "0px"
+        },
+        button: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "500%",
+            height: "100%",
+            // height: "100px",
+            aspectRatio: "1",
+            border: "none",
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.secondary.main,
+        }
     }
 
     let statusIcon,
@@ -21,18 +63,22 @@ const UserActionButton = (props) => {
         statusIconCommonProps = { className: "UserActionButton_icon" },
         isDisabled = false;
 
-    const [mediaRecorder, setMediaRecorder] = useState(null);
+    // const [mediaRecorder, setMediaRecorder] = useState(null);
+    const recorder = useAudioRecorder();
 
     const onClick = () => {
         if (props.status === "idle") {
-            navigator.mediaDevices.getUserMedia({ audio: true }).then((mediaStream) => {
-                console.log('setting media recorder')
-                setMediaRecorder(new MediaRecorder(mediaStream));
-            })
+            recorder.startRecording()
+
+            // navigator.mediaDevices.getUserMedia({ audio: true }).then((mediaStream) => {
+            //     console.log('setting media recorder')
+            //     setMediaRecorder(new MediaRecorder(mediaStream));
+            // })
         }
         else {
-            mediaRecorder.stop();
-            setMediaRecorder(null);
+            // mediaRecorder.stop();
+            // setMediaRecorder(null);
+            recorder.stopRecording()
         }
         props.onClick();
     }
@@ -61,17 +107,28 @@ const UserActionButton = (props) => {
 
     return (
         <>
-            <div style={styles}>
-                {mediaRecorder && <LiveAudioVisualizer mediaRecorder={mediaRecorder} width={200} height={75}/>}
-                <button
-                    className={`UserActionButton UserActionButton-${props.status} ${
-                        isDisabled ? "disabled" : ""
-                    } mb-2`}
-                    onClick={onClick}
-                    disabled={isDisabled}
-                >
-                    {statusIcon}
-                </button>
+            <div style={styles.container}>
+                <div style={styles.recorder}>
+                    <button
+                        style={styles.button}
+                        onClick={onClick}
+                        disabled={isDisabled}
+                    >
+                        {statusIcon}
+                    </button>
+                        <div style={styles.recorderWaveformContainer}>
+                            {recorder.mediaRecorder && 
+                                <LiveAudioVisualizer mediaRecorder={recorder.mediaRecorder} 
+                                        // blob={blob}
+                                    width={400}
+                                    height={200}
+                                    barWidth={2}
+                                    gap={3}
+                                    barColor={'#f76565'}
+                                />
+                            }
+                        </div>
+                </div>
                 <Typography variant="h4" >{statusText}</Typography>
             </div>
         </>
